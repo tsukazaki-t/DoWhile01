46081a5bbb15f3886d19ba39563af2491e880158
TomomiTsukazaki <122886012+tsukazaki-t@users.noreply.github.com>
2023-02-12 22:15:48 +0900

    課題提出

---
+++ src/DoWhile01.java
@@ -0,0 +1,19 @@
+
+public class DoWhile01 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		
+		int number = 1;
+		
+		do {
+			number *= 2;
+			System.out.println(number);
+			
+		}while(number < 50);
+		
+		
+
+	}
+
+}
